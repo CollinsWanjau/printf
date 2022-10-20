53ce84fa23fbcd4aa1600fb29b7122dc3f59739d
colloso999 <tripperskripper@gmail.com>
2022-10-20 10:16:40 -0700

    custom printf

---
+++ _printf.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * vprintk - print formatted data from variable args
+ *
+ * state:
+ *  0: regular
+ *  1: escape
+ */
+void vprintk(const char *fmt, va_list args)
+{
+  int state = 0;
+
+  while (*fmt)
+  {
+    if (state == 0)
+    {
+      if (*fmt == '%')
+      {
+        state = 1;
+      }
+      else
+      {
+        putchar(*fmt);
+      }
+    }
+    else if (state == 1)
+    {
+      switch (*fmt)
+      {
+      case 'c':
+      {
+        char ch = va_arg(args, int);
+        putchar(ch);
+        break;
+      }
+      case 's':
+      {
+        const char *s = va_arg(args, const char *);
+        while (*s)
+        {
+          putchar(*s++);
+        }
+        break;
+      }
+      }
+
+      state = 0;
+    }
+
+    fmt++;
+  }
+}
+int _printf(const char *fmt, ...)
+{
+  va_list args;
+  va_start(args, fmt);
+
+  vprintk(fmt, args);
+
+  va_end(args);
+}
+
+int main()
+{
+  void *pointer_to_main = (void *)main;
+
+  _printf("Print a char: %c. Done\n", 'H');
+  _printf("Print a string: %s. Done\n", "Hello world");
+
+  return (0);
+}
